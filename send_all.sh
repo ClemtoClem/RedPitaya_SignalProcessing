0fab06f1993ab2056625493ba72a01a8b0a41500
Clem <clement01.charriere@gmail.com>
2024-08-29 14:16:34 +0200

    module

--- send_all.sh
+++ send_all.sh
@@ -2,5 +2,5 @@
 # Executer le script envoyer le contenue du répertoire à la RedPitaya
 
 scp -r ./src root@rp-f0baa5.local:/root/Programs/RedPitaya_SignalProcessing/
-scp -r ./Makfile root@rp-f0baa5.local:/root/Programs/RedPitaya_SignalProcessing/
+scp Makefile root@rp-f0baa5.local:/root/Programs/RedPitaya_SignalProcessing/
 scp -r ./compile.sh root@rp-f0baa5.local:/root/Programs/RedPitaya_SignalProcessing/
